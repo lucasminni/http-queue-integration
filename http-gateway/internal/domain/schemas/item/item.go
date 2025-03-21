3fe4560acb8737e91532bb9ed7be707a1c0c3aa1
minni <lucasminni@hotmail.com>
2025-03-21 20:59:15 -0300

    adicionando schemas basicos

--- http-gateway/internal/domain/schemas/item/item.go
+++ http-gateway/internal/domain/schemas/item/item.go
@@ -1,7 +1,11 @@
 package schemas
 
 type BodyNewItem struct {
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
 }
 
 type BodyUpdateItem struct {
+	Name  string  `json:"new_name"`
+	Price float64 `json:"new_price"`
 }
--- http-gateway/internal/domain/schemas/order/order.go
+++ http-gateway/internal/domain/schemas/order/order.go
@@ -1,7 +1,20 @@
 package schemas
 
+import (
+	models "http-gateway/internal/domain/models/item"
+	"time"
+)
+
 type BodyNewOrder struct {
+	OrderDate  time.Time     `json:"order_date"`
+	Status     string        `json:"status"`
+	Items      []models.Item `json:"items"`
+	TotalPrice float64       `json:"total_price"`
 }
 
 type BodyUpdateOrder struct {
+	OrderDate  time.Time     `json:"order_date"`
+	Status     string        `json:"status"`
+	Items      []models.Item `json:"items"`
+	TotalPrice float64       `json:"total_price"`
 }
